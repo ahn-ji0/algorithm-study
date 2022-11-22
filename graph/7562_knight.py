7cb00aeda524925ddec7e0af6f2ec93f3219f49a
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-22 15:17:16 +0900

    7562 - 나이트의 이동 : 무한 루프 fix, 시간 초과

--- graph/7562_knight.py
+++ graph/7562_knight.py
@@ -2,23 +2,31 @@
 # 
 dx = [1,2,2,1,-1,-2,-2,-1]
 dy = [2,1,-1,-2,-2,-1,1,2]
-cnt = []
-def solution(l,from_coord,to_coord,count):
-    if(from_coord == to_coord):
+
+def solution(l,passed,to_coord,count):
+    global cnt
+    
+    if(passed[-1] == to_coord):
         cnt.append(count-1)
         return
     
-    x, y = from_coord
+    x, y = passed[-1]
     for i in range(len(dx)):
         x_tmp = x + dx[i]
         y_tmp = y + dy[i]
-        if(x_tmp < 0 or x_tmp >= l or y_tmp < 0 or y_tmp >= l):
-            return
-        solution(l,[x_tmp,y_tmp],to_coord,count+1)
 
-    
+        if([x_tmp,y_tmp] in passed or x_tmp < 0 or x_tmp >= l or y_tmp < 0 or y_tmp >= l):
+            return
+        passed.append([x_tmp,y_tmp])
+        solution(l,passed,to_coord,count+1)
+        passed.pop()
+
+# cases = int(input())
+# for case in range(cases):
+global cnt
+cnt = []
 l = int(input())
 from_coord = list(map(int,input().split()))
 to_coord= list(map(int,input().split()))
-solution(l,from_coord,to_coord,1)
+solution(l,[from_coord],to_coord,1)
 print(min(cnt))
