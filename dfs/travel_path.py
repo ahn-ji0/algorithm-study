7b5d23923f8784eb28b41f8e90684dddb63d5d34
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-28 19:38:55 +0900

    여행경로 풀이 통과 완료

--- dfs/travel_path.py
+++ dfs/travel_path.py
@@ -1,33 +1,35 @@
 # 프로그래머스 - 여행경로 https://school.programmers.co.kr/learn/courses/30/lessons/43164
-def dfs(start, tickets):
-    
-    path.append(start)
+# 성공 - 테스트 케이스 여러가지 생각해 볼 필요 있는 문제
 
-    if sum(used)==len(tickets):
+total = []
+
+def dfs(start, tickets, path):
+    
+    if len(path)==len(tickets)+1:
+        # total.append(path)만 해주면 path가 변함에 따라 total 내의 값도 변함. 즉 path를 참조하지 않고 복제한 것을 append 해야 함.
+        total.append(list(path))
         return
 
-    min_end = "ZZZ"
-    min_idx = 0
 
     for i in range(len(tickets)):
         if used[i]==0 and tickets[i][0]==start:
-            if tickets[i][1] <= min_end:
-                min_end = tickets[i][1]
-                min_idx = i
+            used[i] = 1
+            path.append(tickets[i][1])
+            dfs(tickets[i][1],tickets,path)
+            path.pop()
+            used[i] = 0
 
-    used[min_idx] = 1
-    end = min_end
-    dfs(end,tickets)
     
 def solution(tickets):
     global used
-    global path
-
-    used = [0 for i in range(len(tickets))]
-    path = list()
     
-    dfs("ICN",tickets)
-    return path
+    used = [0 for i in range(len(tickets))]
+    
+    dfs("ICN",tickets,["ICN"])
+    
+    total.sort()
+    
+    return total[0]
 
-tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
 print(solution(tickets))
