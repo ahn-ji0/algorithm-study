8cedb433f0a3191372580fb2473fcc80ba83d64c
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-12 10:30:05 +0900

    리팩토링, math import

--- greedy/base_station.py
+++ greedy/base_station.py
@@ -1,24 +1,21 @@
 # 프로그래머스 - 기지국 설치 https://school.programmers.co.kr/learn/courses/30/lessons/12979
 
+import math
+
 def solution(n, stations, w):
     no_stations =  []
     from_ = 1
-    
+    count = 0
+
     for i in range(len(stations)):
         to = stations[i] - w - 1
-        no_stations.append(to - from_ + 1)
+        empty = to - from_ + 1
+        count += math.ceil(empty / (2 * w + 1))
         from_ = stations[i] + w + 1
     
     if from_ <= n :
-        no_stations.append(n - from_ + 1)
-        
-    count = 0
-    
-    for empty in no_stations:
-        if empty % (2 * w + 1) == 0 :
-            count += empty // (2 * w + 1)
-        else:
-            count += empty // (2 * w + 1) + 1
+        empty = n - from_ + 1
+        count += math.ceil(empty / (2 * w + 1))
         
     return count
 
