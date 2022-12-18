bf41edd399b431d87a3ad2572e8a31f52b78daaa
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-18 23:13:01 +0900

    1. 문자열 포함 여부 확인

---
+++ hash/phone_number.py
@@ -0,0 +1,22 @@
+# 프로그래머스 - 전화번호 목록 https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3
+# 
+
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    
+    for i in range(len(phone_book)):
+        for j in range(i+1,len(phone_book)):
+            if phone_book[i] in phone_book[j]:
+                return False
+            
+    return answer
+
+phone_book = ["119", "97674223", "1195524421"]
+print(solution(phone_book))
+
+phone_book = ["123","456","789"]
+print(solution(phone_book))
+
+phone_book = ["12","123","1235","567","88"]
+print(solution(phone_book))
