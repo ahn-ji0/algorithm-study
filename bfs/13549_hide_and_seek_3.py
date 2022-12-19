1546fd5a39a5363f1eaac582e2310c1c8598499e
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-19 19:44:15 +0900

    13549 - 1.일부 테스트 케이스 통과

---
+++ bfs/13549_hide_and_seek_3.py
@@ -0,0 +1,26 @@
+# 13549 숨바꼭질 3 https://www.acmicpc.net/problem/1697
+
+from collections import deque
+
+def solution(start, end):
+    queue = deque([(start, 0)])
+    
+    visited = [False] * 1000001
+    
+    while queue:
+        x, depth = queue.popleft()
+        
+        if x == end:
+            return depth
+        
+        visited[x] = True
+        
+        for tmp_x in (x+1, x-1, 2*x):
+            
+            if tmp_x < 0 or tmp_x > 100000 or visited[tmp_x] == True:
+                continue
+            
+            queue.append((tmp_x, depth)) if tmp_x == 2*x else queue.append((tmp_x, depth + 1))
+            
+start, end = map(int, input().split())
+print(solution(start, end))
