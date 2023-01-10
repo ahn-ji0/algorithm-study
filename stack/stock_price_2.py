48046edf4de50119e34358db0accebddc79bd283
Jiyoung <ahn.jiyoung15@gmail.com>
2023-01-10 17:35:23 +0900

    주식가격 - 완전탐색(성공)

---
+++ stack/stock_price_2.py
@@ -0,0 +1,16 @@
+# 프로그래머스 주식 가격 https://school.programmers.co.kr/learn/courses/30/lessons/42584
+# 해시 - 시간초과
+
+from collections import deque
+
+def solution(prices):
+    N = len(prices)
+    answer = [0] * N
+    for i in range(N):
+        for j in range(i+1, N):
+            answer[i] += 1
+            if prices[i] > prices[j]:
+                break
+    return answer       
+
+print(solution([1,2,3,2,3]))
